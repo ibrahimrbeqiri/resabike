02e16f3f3afdb412491e58adc3bdf1bb6e21d868
Mikko Lerto <mikko.lerto@astehelsinki.fi>
2017-10-05 09:57:33 +0200

    link

--- header.php
+++ header.php
@@ -22,8 +22,8 @@
   <div class="nav-wrapper yellow darken-1">
 	<a href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" class="brand-logo">Resabike</a>
 	<ul id="nav-mobile" class="right hide-on-med-and-down">
-	  <li><a href="sass.html">Reserve</a></li>
-	  <li><a href="badges.html">Contact</a></li>
+	  <li><a href="reserve.php">Reserve</a></li>
+	  <li><a href="#">Contact</a></li>
 	</ul>
   </div>
 </nav>
