36d20e6d1dbf39e5b9bb999d59802b36e64fbddf
Ibrahim Beqiri <ibraa.cs@msn.com>
2017-10-19 10:41:09 +0200

    ...

--- models/Class.Line.php
+++ models/Class.Line.php
@@ -1,5 +1,6 @@
 <?php
-namespace models;
+
+use User;
 
 class Line
 {
@@ -8,8 +9,73 @@ class Line
     private $number;
     private $fromstation;
     private $tostation;
+    private $driver;
+    
 
     public function __construct()
-    {}
+    {
+        
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    public function getFromstation()
+    {
+        return $this->fromstation;
+    }
+
+    public function getTostation()
+    {
+        return $this->tostation;
+    }
+
+    public function getDriver(User $driver)
+    {
+        return $this->driver;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    public function setFromstation($fromstation)
+    {
+        $this->fromstation = $fromstation;
+    }
+
+    public function setTostation($tostation)
+    {
+        $this->tostation = $tostation;
+    }
+
+    public function setDriverId($driverId)
+    {
+        $this->driverId = $driverId;
+    }
+
 }
 
