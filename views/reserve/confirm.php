9df77157bc43cdad11af9607f6a869089be38c25
Mikko Lerto <mikko.lerto@astehelsinki.fi>
2017-10-23 09:30:08 +0200

    confirm

--- views/reserve/confirm.php
+++ views/reserve/confirm.php
@@ -16,23 +16,34 @@ $reservationdate = $_SESSION['reservationArray']['reservationdate'];
 $departure = $_SESSION['reservationArray']['departure'];
 $arrival = $_SESSION['reservationArray']['arrival'];
 
-var_dump($_SESSION['reservationArray']);
+//var_dump($_SESSION['reservationArray']);
 ?>
 
 <div class="container">
 	<div class="row">
-		
+		<h3>Confirm your reservation</h3>
+		<p>Date: <?php echo $reservationdate ?></p>
+		<p>From: <?php echo $fromstation." ".$departure ?></p>
+		<p>To: <?php echo $tostation." ".$arrival ?></p>
+		<p>Number of bikes: <?php echo $bikenumber ?></p>
+		<p>Firstname: <?php echo $firstname ?></p>
+		<p>Lastname: <?php echo $lastname ?></p>
+		<p>Email: <?php echo $email ?></p>
+		<p>Phone: <?php echo $phone ?></p>
+		<p>Remarks: <?php echo $remarks ?></p>
+		<p></p>
+
 
 
 		<a href="<?php echo URL_DIR.'reserve/confirmed';?>">
 			<button class="btn waves-effect waves-light" type="button">
-				<i class="material-icons prefix">map</i>
+				<i class="material-icons prefix">check</i>
 				<span>Confirm</span>
 			</button>
 		</a>
 		<a href="<?php echo URL_DIR.'reserve/cancel';?>">
 			<button class="btn waves-effect waves-light" type="button">
-				<i class="material-icons prefix">map</i>
+				<i class="material-icons prefix">cancel</i>
 				<span>Cancel</span>
 			</button>
 		</a>
