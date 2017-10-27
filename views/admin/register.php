a80e18b7f1716a9c949ce4421b7c384452051bac
Mikko Lerto <mikko.lerto@astehelsinki.fi>
2017-10-27 10:43:43 +0200

    bug fix

--- views/admin/register.php
+++ views/admin/register.php
@@ -15,48 +15,56 @@ $roles = $_SESSION['user_roles'];
 			<h3 class="left">Register new user</h3>
 		</div>
 		<form class="col s12">
-
-
-			<div class="input-field col s6 l6">
-				<i class="material-icons prefix">person</i>
-			  <input type="text" class="validate">
-			  <label for="first_name">First Name</label>
-			</div>
-			<div class="input-field col s6 l6">
-			  <input type="text" class="validate">
-			  <label for="last_name">Last Name</label>
-			</div>
-
-			<div class="input-field col s6 l6">
-				<i class="material-icons prefix">vpn_key</i>
-			  <input type="password" class="validate">
-			  <label for="password">Password</label>
-			</div>
-			<div class="input-field col s6 l6">
-			  <input type="password" class="validate">
-			  <label for="password">confirm password</label>
+			<div class="col l12">
+				<div class="input-field col s6 l6">
+					<i class="material-icons prefix">person</i>
+				  <input type="text" class="validate">
+				  <label for="first_name">First Name</label>
+				</div>
+				<div class="input-field col s6 l6">
+				  <input type="text" class="validate">
+				  <label for="last_name">Last Name</label>
+				</div>
 			</div>
 
-			<div class="input-field col s6 l6">
-				<i class="material-icons prefix">mail</i>
-			  <input type="email" class="validate">
-			  <label for="email">Email</label>
+			<div class="col l12">
+				<div class="input-field col s6 l6">
+					<i class="material-icons prefix">vpn_key</i>
+				  <input type="password" class="validate">
+				  <label for="password">Password</label>
+				</div>
+				<div class="input-field col s6 l6">
+				  <input type="password" class="validate">
+				  <label for="password">confirm password</label>
+				</div>
 			</div>
 
-			<div class="input-field col s6 l6">
-			  <select>
-				  <option disabled selected>User role</option>
-				  <?php foreach ($roles as $role): ?>
-					  <option value="<?php echo $role[id]; ?>"><?php echo $role[role]; ?></option>
-				  <?php endforeach; ?>
-			  </select>
+			<div class="col l12">
+				<div class="input-field col s6 l6">
+					<i class="material-icons prefix">mail</i>
+				  <input type="email" class="validate">
+				  <label for="email">Email</label>
+				</div>
+
+				<div class="input-field col s6 l6">
+				  <select>
+					  <option disabled selected>User role</option>
+					  <?php foreach ($roles as $role): ?>
+						  <option value="<?php echo $role[id]; ?>"><?php echo $role[role]; ?></option>
+					  <?php endforeach; ?>
+				  </select>
+				</div>
 			</div>
 
-			<div class="input-field col s12 l6">
-				<i class="material-icons prefix">phone</i>
-			  <input type="tel" class="validate">
-			  <label for="email">Phone number</label>
+			<div class="col l12">
+				<div class="input-field col s12 l6">
+					<i class="material-icons prefix">phone</i>
+				  <input type="tel" class="validate">
+				  <label for="email">Phone number</label>
+				</div>
 			</div>
+
+
 			<div class="col s12">
 				<div class="row">
 					<div class="col s6">
