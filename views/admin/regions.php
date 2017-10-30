d90e54059a7aa2f7e589b642242ace1d6f4d7297
Mikko Lerto <mikko.lerto@astehelsinki.fi>
2017-10-30 10:00:11 +0100

    div table

--- views/admin/regions.php
+++ views/admin/regions.php
@@ -13,34 +13,37 @@ $regions = $_SESSION['regions'];
 	<div class="col l12 center card admin-menu">
 		<h4>Regions:</h4>
 		<p class="<?php echo URL_DIR.'admin/regions/save';?>">Disclamer: Make sure the zone IDs are correct!</p>
-		<form action="<?php echo URL_DIR.'admin/editRegions';?>" method="post">
+
 
 			<a href="<?php echo URL_DIR.'admin/menu';?>">
 				<button class="btn waves-effect waves-light right" type="button">Cancel
 					<i class="material-icons left">cancel</i>
 				</button>
 			</a>
+			<thead>
+				<th></th>
+				<th>Region ID</th>
+				<th>Region Name</th>
+			</thead>
+			<div class="table" id="regions-list">
 
-			<table id="regions-list">
-				<thead>
-					<th></th>
-					<th>Region ID</th>
-					<th>Region Name</th>
-				</thead>
-				<tbody>
+
 					<?php foreach ($regions as $region): ?>
-					<tr>
-							<td><button class="btn-floating" type="submit" name="modify"><i class="material-icons">save</i></button></td>
-							<td><p><?php echo $region['regionId'] ?></p> <input type="text" name="id" hidden value="<?php echo $region['regionId'] ?>"></td>
-							<td><input type="text" name="name" value="<?php echo $region['regionName'] ?>"></td>
-							<td><button class="btn-floating" type="submit" name="delete"><i class="material-icons">delete</i></button></td>
+						<form action="<?php echo URL_DIR.'admin/editRegions';?>" method="post">
 
-					</tr>
+						<div class="table-row">
+							<div class="table-cell"><button class="btn-floating" type="submit" name="modify"><i class="material-icons">save</i></button></div>
+							<div class="table-cell"><p><?php echo $region['regionId'] ?></p> <input type="text" name="id" hidden value="<?php echo $region['regionId'] ?>"></div>
+							<div class="table-cell"><input type="text" name="name" value="<?php echo $region['regionName'] ?>"></div>
+							<div class="table-cell"><button class="btn-floating" type="submit" name="delete"><i class="material-icons">delete</i></button></div>
+						</div>
+
+
+					</form>
 					<?php endforeach; ?>
-				</tbody>
 
-			</table>
-		</form>
+			</div>
+
 
 		<button id="add-table-row" class="btn waves-effect waves-light left" type="button" name="action">Add region
 	  		<i class="material-icons right">add</i>
