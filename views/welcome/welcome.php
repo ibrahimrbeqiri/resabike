aff13d2d5967182ec616f40e18c9858889eb5649
Mikko Lerto <mikko.lerto@astehelsinki.fi>
2017-11-03 11:27:03 +0100

    only busses allowed

--- views/welcome/welcome.php
+++ views/welcome/welcome.php
@@ -44,25 +44,25 @@ $msg = $this->vars['msg'];
 			</div>
 			<div class="col s12 m4">
 			  <div class="center promo">
-				<i class="material-icons">flash_on</i>
-				<p class="promo-caption">Speeds up development</p>
-				<p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+				<i class="material-icons">search</i>
+				<p class="promo-caption"> 1. Search for bus routes</p>
+				<p class="light center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			  </div>
 			</div>
 
 			<div class="col s12 m4">
 			  <div class="center promo">
-				<i class="material-icons">group</i>
-				<p class="promo-caption">User Experience Focused</p>
-				<p class="light center">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+				<i class="material-icons">directions_bike</i>
+				<p class="promo-caption">2. Reserve bikes for bus route</p>
+				<p class="light center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			  </div>
 			</div>
 
 			<div class="col s12 m4">
 			  <div class="center promo">
-				<i class="material-icons">settings</i>
-				<p class="promo-caption">Easy to work with</p>
-				<p class="light center">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+				<i class="material-icons">mood</i>
+				<p class="promo-caption">3. Enjoy</p>
+				<p class="light center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			  </div>
 			</div>
 		</div>
